a2e7f0c2b02a1c1323c8bda0686576b26e41431f
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2023-12-24 10:28:23 -0300

    feat: adiciona uso do socket.io

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,4 +1,26 @@
-import { Game } from "./classes/game.js";
+import { Fruit, Game, KeyBoard, Player } from "./classes/index.js";
 
 const game = new Game();
+game.keyboard = new KeyBoard();
+game.keyboard.subscribeObserver(game.movePlayer.bind(game));
 game.update();
+
+const socket = window.io();
+
+socket.on("connect", () => {
+  console.log(`Player connected on Client with id ${socket.id}`);
+  game.playerId = socket.id;
+});
+
+socket.on("setup", (gameState) => {
+  console.log(gameState);
+  for (const playerId in gameState.players) {
+    const player = gameState.players[playerId];
+    game.addPlayer(new Player(player));
+  }
+
+  for (const fruitId in gameState.fruits) {
+    const fruit = gameState.fruits[fruitId];
+    game.addPlayer(new Fruit(fruit));
+  }
+});
