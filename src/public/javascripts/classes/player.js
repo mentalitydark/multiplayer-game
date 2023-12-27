00cbe40e4f6857b78f79f0df857874bed72cc12b
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2023-12-26 22:23:19 -0300

    feat: adiciona prop name na classe

--- src/public/javascripts/classes/player.js
+++ src/public/javascripts/classes/player.js
@@ -5,15 +5,21 @@ import { Entity } from "./entity.js";
  */
 export class Player extends Entity {
 
+  /** @type {string} */
+  name;
+
   /**
    * @param {({
   * id: string
   * position: { x: number, y: number }
   * color?: string
+  * name?: string
   * })} props
   */
   constructor(props) {
     props.color = "color" in props ? props.color : "gray";
     super(props);
+    
+    this.name = "name" in props ? props.name : props.id;
   }
 }
